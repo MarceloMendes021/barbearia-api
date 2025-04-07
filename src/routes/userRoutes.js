c496ff208c11bdd3649efa1a8cf30343ba32f9f4
Marcelo Mendes <marcelotavares.96@gmail.com>
2025-04-07 14:26:51 -0300

    feat(routes): protege rota de perfil com middleware JWT

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
 const { registerUser } = require("../controllers/userController");
+const { getUserProfile } = require("../controllers/authController");
+const { verifyToken } = require("../middlewares/authMiddleaware");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.get("/me", verifyToken, getUserProfile);
 
 module.exports = router;
