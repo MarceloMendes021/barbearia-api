9ac64f013fb10578f70758b81fb48af9352f9a6b
Marcelo Mendes <marcelotavares.96@gmail.com>
2025-04-07 14:29:05 -0300

    chore(server): habilitar CORS para requisições do front

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 mongoose
